25178a31186013bdbbcdeeda604081ceb4cbfacc
Zerosaiky <157794272+Zerosaiky@users.noreply.github.com>
2024-04-11 18:49:30 +0500

    Update app.js

--- app.js
+++ app.js
@@ -55,13 +55,3 @@ btn2.addEventListener("click", function(){
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
-
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
